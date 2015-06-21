5a5a2ce0415a81df3b9425cb4ea8a331bea281c2
Maziar <mazi52005@gmail.com>
2015-06-20 21:24:04 -0400

    test url call

--- src/main/java/com/appdirect/integration/rest/SubscriptionResource.java
+++ src/main/java/com/appdirect/integration/rest/SubscriptionResource.java
@@ -1,8 +1,14 @@
 package com.appdirect.integration.rest;
 
+import java.util.Map;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+
+import com.sun.jersey.api.core.HttpContext;
 
 @Path("/event/")
 public class SubscriptionResource {
@@ -10,7 +16,7 @@ public class SubscriptionResource {
 	@GET
 	@Produces("text/plain")
 	@Path("/test")
-	public String testing(){
-		return "Good call";
+	public String testing(@Context HttpContext context){
+		return context.getRequest().getRequestUri().toASCIIString();
 	}
 }
